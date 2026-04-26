726d349b069131e301ef1f153738048eeb21c919
eyedeekay <hankhill19580@gmail.com>
2026-04-25 23:09:17 -0400

    refactor: enhance Start method in I2PBrowser to check for existing tunnel and listener

--- i2pbrowser.go
+++ i2pbrowser.go
@@ -1,6 +1,8 @@
 package goi2pbrowser
 
 import (
+	"net"
+
 	"github.com/go-i2p/go-i2ptunnel-config/i2pconv"
 	embedding "github.com/go-i2p/go-i2ptunnel/lib/embedding"
 	httpclient "github.com/go-i2p/go-i2ptunnel/lib/http/client"
@@ -9,6 +11,26 @@ import (
 type I2PBrowser struct {
 	ProfileDir string
 	*embedding.Tunnel
+}
+
+func (i *I2PBrowser) Start() error {
+	// only start the tunnel if it is not already running
+	if i.Tunnel != nil && i.Tunnel.IsRunning() {
+		return nil
+	}
+	// also check for a listener on the tunnels configured port before starting the tunnel
+	t := i.Tunnel.Tunnel()
+	addr, err := t.LocalAddress()
+	if err != nil {
+		return err
+	}
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		// proceed anyway in this case, it means that I2P is already running and the tunnel is already running, so we can just return nil
+		return nil
+	}
+	ln.Close()
+	return i.Tunnel.Start()
 }
 
 func (i *I2PBrowser) Stop() error {
